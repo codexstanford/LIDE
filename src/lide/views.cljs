c188f4832fe199153ea5779ca916a7fded82d26e
William O'Hanley <me@wohanley.com>
2022-04-19 18:07:31 -0700

    Correct some terminology

--- src/lide/views.cljs
+++ src/lide/views.cljs
@@ -6,73 +6,70 @@
    ))
 
 (def program
-  [{:name "applies"
-    :args ["RD" "ComplianceOption"]
-    :conj [{:predicate "rd_type"
+  [{:head {:predicate "applies"
+           :args ["RD" "ComplianceOption"]}
+    :body [{:predicate "rd_type"
             :args ["RD" "RDType"]}
            {:predicate "rd_iu_type"
             :args ["RD" "IUType"]}
            {:predicate "rd_iu_location"
             :args ["RD" "IULocation"]}]}
-   {:name "rd_type"
-    :args ["RD" "Type"]}
-   {:name "rd_iu_type"
-    :args ["RD" "IUType"]}
-   {:name "rd_iu_location"
-    :args ["RD" "IULocation"]}])
+   {:head {:predicate "rd_type"
+           :args ["RD" "Type"]}}
+   {:head {:predicate "rd_iu_type"
+           :args ["RD" "IUType"]}}
+   {:head {:predicate "rd_iu_location"
+           :args ["RD" "IULocation"]}}])
 
 (defn literal-to-epilog [literal]
-  (str (:predicate clause)
+  (str (:predicate literal)
        "("
-       (string/join ", " (:args clause))
+       (string/join ", " (:args literal))
        ")"))
 
-(defn predicate-to-epilog [predicate]
-  (let [head (str (:name predicate)
-                  "("
-                  (string/join ", " (:args predicate))
-                  ")")
-        body (when (seq (:conj predicate))
-               (string/join "\n\t& " (map literal-to-epilog (:conj predicate))))]
+(defn rule-to-epilog [rule]
+  (let [head (literal-to-epilog (:head rule))
+        body (when (seq (:body rule))
+               (string/join "\n\t& " (map literal-to-epilog (:body rule))))]
     (str head
          (when body
            (str " :-\n\t" body)))))
 
-(defn predicates-view-model [program]
+(defn rules-view-model [program]
   ;; TODO support disjunction - we might already have a definition
   (map
-   (fn [predicate]
-     (let [internals (->> predicate
-                          :conj
+   (fn [rule]
+     (let [internals (->> rule
+                          :body
                           (mapcat :args)
                           (filter (fn [internal]
-                                    (not (some #(= internal %) (:args predicate)))))
+                                    (not (some #(= internal %) (-> rule :head :args)))))
                           vec)]
-       (into {} [(select-keys predicate [:name :args])
-                 {:internals internals}])))
+       {:head (:head rule)
+        :internals internals}))
    program))
 
 (defn connections-view-model [program]
-  (let [predicates-by-name (->> program
-                                (map #(vector (:name %) %))
+  (let [rules-by-head-pred (->> program
+                                (map #(vector (-> % :head :predicate) %))
                                 (into {}))
         connections-nested (map
-                            (fn [predicate]
+                            (fn [rule]
                               (map
-                               (fn [conjunction]
+                               (fn [body-literal]
                                  (map-indexed
                                   (fn [i arg]
                                     [i arg]
-                                    {:src  [(:predicate conjunction)
-                                            (as-> conjunction x
+                                    {:src  [(:predicate body-literal)
+                                            (as-> body-literal x
                                               (:predicate x)
-                                              (get predicates-by-name x)
-                                              (:args x)
+                                              (get rules-by-head-pred x)
+                                              (-> x :head :args)
                                               (get x i))]
-                                     :dest [(:name predicate)
+                                     :dest [(-> rule :head :predicate)
                                             arg]})
-                                  (:args conjunction)))
-                               (:conj predicate)))
+                                  (:args body-literal)))
+                               (:body rule)))
                             program)]
     (->> connections-nested
          flatten
@@ -84,17 +81,18 @@
    "rd_iu_type" {:x 220 :y 80}
    "rd_iu_location" {:x 220 :y 160}})
 
-(def rule-name-font-size 18)
-(def rule-name-padding 6)
+(def rule-head-font-size 18)
+(def rule-head-padding 6)
 (def rule-binding-font-size 16)
 (def rule-binding-padding-x 6)
 (def rule-binding-padding-y 3)
 
 (defn rule-layout [rule]
-  (let [name-height (+ rule-name-padding
-                       rule-name-font-size
-                       rule-name-padding)
-        name {:position {:x rule-name-padding
+  (let [name-height (+ rule-head-padding
+                       rule-head-font-size
+                       rule-head-padding)
+        head {:predicate (-> rule :head :predicate)
+              :position {:x rule-head-padding
                          :y (/ name-height 2)}
               :size     {:width  150
                          :height name-height}}
@@ -108,8 +106,8 @@
                 [arg
                  {:position {:y (+ args-y-start
                                    (* i arg-height))}}])
-              (:args rule))
-        args-height (* arg-height (->> rule :args count))
+              (-> rule :head :args))
+        args-height (* arg-height (-> rule :head :args count))
         internals-y-start (+ name-height
                              args-height
                              (/ arg-height 2))
@@ -120,22 +118,22 @@
                                         (* i arg-height))}}])
                    (:internals rule))
         internals-height (* arg-height (->> rule :internals count))]
-    {:name name
+    {:head head
      :args (into {} args)
      :internals (into {} internals)
-     :container {:position (get rule-positions (:name rule) {:x 0 :y 0})
+     :container {:position (get rule-positions (-> rule :head :predicate) {:x 0 :y 0})
                  :size {:width  150
                         :height (+ name-height args-height internals-height)}}}))
 
 (defn rule [{:keys [rule layout]}]
   [:g {:transform (str "translate(" (-> layout :container :position :x) "," (-> layout :container :position :y) ")")
-       :key (:name rule)}
+       :key (:head rule)}
    [:rect {:class  "rule__bg"
            :width  (->> layout :container :size :width)
            :height (->> layout :container :size :height)}]
-   [:text {:x rule-name-padding
-           :y (/ (+ rule-name-padding rule-name-font-size rule-name-padding) 2)}
-    (:name rule)]
+   [:text {:x rule-head-padding
+           :y (/ (+ rule-head-padding rule-head-font-size rule-head-padding) 2)}
+    (-> rule :head :predicate)]
    [:<>
     (map
      (fn [[arg arg-layout]]
@@ -167,6 +165,8 @@
         end-layout (get rule-layouts end-rule)
         start (socket-position start-layout start-value {:end :src})
         end (socket-position end-layout end-value {:end :dest})]
+    (println start-layout)
+    (println end-layout)
     [:line {:x1 (:x start)
             :y1 (:y start)
             :x2 (:x end)
@@ -174,14 +174,14 @@
             :stroke "black"}]))
 
 (defn main-panel []
-  (let [predicates-vm (predicates-view-model program)
-        predicate-vms-by-name (->> predicates-vm
-                                   (map #(vector (:name %) %))
-                                   (into {}))
-        rule-layouts (->> predicate-vms-by-name
+  (let [rules-vm (rules-view-model program)
+        rule-vms-by-head (->> rules-vm
+                              (map #(vector (-> % :head :predicate) %))
+                              (into {}))
+        rule-layouts (->> rule-vms-by-head
                           (map
-                           (fn [[name pred]]
-                             [name (rule-layout pred)]))
+                           (fn [[head pred]]
+                             [head (rule-layout pred)]))
                           (into {}))
         connections-vm (connections-view-model program)]
     [:svg {:height 500
@@ -190,8 +190,8 @@
              :height 500
              :width  1000}]
      (map #(rule {:rule  %
-                  :layout (get rule-layouts (:name %))})
-          predicates-vm)
+                  :layout (get rule-layouts (-> % :head :predicate))})
+          rules-vm)
      (map #(connection {:connection %
                         :rule-layouts rule-layouts})
           connections-vm)]))
